3a8352080482c19c41478a9d8597928cde2c8d84
owen8877 <owentoget@yeah.net>
2017-01-21 16:33:34 +0800

    Completed visual.

---
+++ city-data/visual/converter.js
@@ -0,0 +1,35 @@
+'use strict'
+
+let file = './Nottingham.json'
+let data = require(file)
+const fs = require('fs')
+
+let X = 1 + data.reduce((acc, cur) => {
+  return (acc > cur.x) ? acc : cur.x
+}, 0)
+let Y = 1 + data.reduce((acc, cur) => {
+  return (acc > cur.y) ? acc : cur.y
+}, 0)
+
+console.log(`${X} ${Y}`)
+
+let tmp = []
+//tmp.push(`${X} ${Y}`)
+
+const typeConv = {
+  'r': 0,
+  'w': 1,
+  'c': 2,
+  'o': 3,
+  'u': 4
+}
+
+for (let d of data) {
+  tmp.push(`${d.x} ${d.y} ${d.progress} ${typeConv[d.type]} ${d.bus ? 'T' : 'F'}`)
+}
+
+/*fs.writeFile(`Nottingham.cinput`, tmp.join('\n'), function (err) {
+  if (err) throw err;
+  console.log(`Wrote Nottingham.cinput`)
+})
+*/
--- city-data/visual/render.js
+++ city-data/visual/render.js
@@ -2,45 +2,144 @@
 
 const D3Node = require('d3-node')
 const fs = require('fs')
+const MD = require('google-material-color')
 
-const files = ['./Nottingham-test']
+const files = ['./Nottingham']
+const config = {
+  scale: 10,
+  padding: 0.05
+}
 
 let data = []
-let d3n = new D3Node()
+
+const colorPalette = (type, progress) => {
+  const baseColor = {
+    'u': 'Grey', //unused
+    'o': 'Green', //open
+    'c': 'Amber', //recreation
+    'w': 'Indigo', //work
+    'r': 'Deep Orange', //residential
+    'wa': 'Grey', //debug use!
+    null: 'Black'
+  }
+  const degreeConv = {
+    1: '200',
+    2: '300',
+    3: '500',
+    0: '100',
+    null: '100'
+  }
+  if (type == 'u')
+    progress = 0
+  return MD.palette[baseColor[type]][degreeConv[progress]]
+}
 
 for (let file of files) {
   data.push({name: file, content: require(file)})
 }
 
-for (let city of data) {
+const draw = (dataSet, width, height, subconfig = {typeOn: true, progressOn: true, busStation: false, hideBlock: false}) => {
+  let d3n = new D3Node()
+
   let svg = d3n.createSVG()
-    .attr("width", 1000)
-    .attr("height", 1000)
-    .append("g")
+    .attr("width", width * config.scale)
+    .attr("height", height * config.scale)
+    .attr("viewBox", `0 0 ${width} ${height}`)
 
-  let rects = svg.selectAll("rect")
-    .data(city.content)
+  // draw background
+  svg
+    .append('rect')
+    .attr('x', 0).attr('y', 0).attr('width', '100%').attr('height', '100%')
+    .attr('fill', '#EEEEEE')
+
+  // draw blocks
+  svg
+    .append('g')
+    .selectAll("rect")
+    .data(dataSet)
     .enter()
-    .append("rect");
-
-  rects
+    .append("rect")
     .attr("x", d => {
-      return d.x
+      return d.y + config.padding
     })
     .attr("y", d => {
-      return d.y
+      return d.x + config.padding
     })
     .attr("width", d => {
-      return 1
+      return 1 - 2 * config.padding
     })
     .attr("height", d => {
-      return 1
+      return 1 - 2 * config.padding
+    })
+    .attr("fill", d => {
+      if (subconfig.hideBlock)
+        return colorPalette('wa', '2')
+      return colorPalette(subconfig.typeOn ? d.type : 'wa', subconfig.progressOn ? d.progress : '3')
     })
 
-  let output = d3n.svgString()
+  // draw bus station
+  if (subconfig.busStation) {
+    svg
+      .append('g')
+      .selectAll('circle')
+      .data(dataSet.filter(item => item.bus))
+      .enter()
+      .append('circle')
+      .attr('cx', d => d.y + 0.7)
+      .attr('cy', d => d.x + 0.3)
+      .attr('r', 0.1)
+      .attr('fill', () => MD.get('Red'))
+  }
 
-  fs.writeFile(`${city.name}.svg`, output, function (err) {
-    if (err) throw err;
-    console.log('文件写入成功');
-  })
+  return d3n.svgString()
+}
+
+for (let city of data) {
+  let width = 1 + city.content.reduce((acc, cur) => (acc > cur.y) ? acc : cur.y, 0),
+    height = 1 + city.content.reduce((acc, cur) => (acc > cur.x) ? acc : cur.x, 0)
+  console.log(width + " " + height)
+
+  let seq = [
+    {
+      typeOn: false,
+      progressOn: false,
+      busStation: false,
+      hideBlock: true
+    },
+    {
+      typeOn: true,
+      progressOn: true,
+      busStation: false,
+      hideBlock: false
+    },
+    {
+      typeOn: false,
+      progressOn: true,
+      busStation: false,
+      hideBlock: false
+    },
+    {
+      typeOn: true,
+      progressOn: false,
+      busStation: false,
+      hideBlock: false
+    },
+    {
+      typeOn: false,
+      progressOn: false,
+      busStation: true,
+      hideBlock: true
+    }
+  ]
+
+  for (let subconfig of seq) {
+    let output = draw(city.content, width, height, subconfig)
+
+    let filenameconfig = Object.keys(subconfig).filter(key => subconfig[key]).join(' ')
+    fs.writeFile(`${city.name} ${filenameconfig}.svg`, output, (err) => {
+      if (err)
+        throw err
+      console.log(`Wrote ${city.name} ${filenameconfig}.svg`)
+    })
+  }
 }
